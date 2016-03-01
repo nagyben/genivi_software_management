1530e8a097f479b2ff36921ef1610e622737900b
Ben Nagy <bnagy4@jaguarlandrover.com>
2016-03-01 12:41:25 -0800

    Changed @dbus registrations for rpyc methods in module_loader_ecu1.py

--- module_loader_ecu1/module_loader_ecu1.py
+++ module_loader_ecu1/module_loader_ecu1.py
@@ -6,32 +6,40 @@
 
 
 
-import gtk
-import dbus
-import dbus.service
-from dbus.mainloop.glib import DBusGMainLoop
+#import gtk
+#import dbus
+#import dbus.service
+#from dbus.mainloop.glib import DBusGMainLoop
 import sys
 import time
 import swm
 
+import rpyc
+
 #
 # ECU Module Loader service
 #
-class ECU1ModuleLoaderService(dbus.service.Object):
+class ECU1ModuleLoaderService(rpyc.Service):
     def __init__(self):
-        bus_name = dbus.service.BusName('org.genivi.module_loader_ecu1', bus=dbus.SessionBus())
-        dbus.service.Object.__init__(self, bus_name, '/org/genivi/module_loader_ecu1')
+        #bus_name = dbus.service.BusName('org.genivi.module_loader_ecu1', bus=dbus.SessionBus())
+        #dbus.service.Object.__init__(self, bus_name, '/org/genivi/module_loader_ecu1')
+        pass
 
-    @dbus.service.method('org.genivi.module_loader_ecu1',
-                         async_callbacks=('send_reply', 'send_error'))
+    #@dbus.service.method('org.genivi.module_loader_ecu1',
+    #                     async_callbacks=('send_reply', 'send_error'))
 
-    def flash_module_firmware(self, 
-                              transaction_id, 
+    def exposed_flash_module_firmware(self, transaction_id, image_path, blacklisted_firmware, allow_downgrade, send_reply, send_error):
+        """ function to expose flash_module_firmware over RPyC
+        """
+        return flash_module_firmware(self, transaction_id, image_path, blacklisted_firmware, allow_downgrade, send_reply, send_error)
+
+    def flash_module_firmware(self,
+                              transaction_id,
                               image_path,
                               blacklisted_firmware,
                               allow_downgrade,
                               send_reply,
-                              send_error): 
+                              send_error):
 
 
         print "Package Manager: Got flash_module_firmware()"
@@ -42,9 +50,10 @@ class ECU1ModuleLoaderService(dbus.service.Object):
         print "---"
 
         # Send back an immediate reply since DBUS
-        # doesn't like python dbus-invoked methods to do 
+        # doesn't like python dbus-invoked methods to do
         # their own calls (nested calls).
         #
+        #FIXME: dbus reply
         send_reply(True)
 
         # Simulate install
@@ -53,25 +62,32 @@ class ECU1ModuleLoaderService(dbus.service.Object):
             sys.stdout.write('.')
             sys.stdout.flush()
             time.sleep(0.1)
-        print  
+        print
         print "Done"
         swm.send_operation_result(transaction_id,
                                   swm.SWM_RES_OK,
                                   "Firmware flashing successful for ecu1. Path: {}".format(image_path))
 
         return None
-        
-    @dbus.service.method('org.genivi.module_loader_ecu1')
-    def get_module_firmware_version(self): 
+
+    #@dbus.service.method('org.genivi.module_loader_ecu1')
+
+    def exposed_get_module_firmware_version(self):
+        """ function to expose get_module_firmware_version over RPyC
+        """
+        return get_module_firmware_version(self)
+
+    def get_module_firmware_version(self):
         print "Got get_installed_packages()"
         return ("ecu1_firmware_1.2.3", 1452904544)
-                 
-print 
+
+print
 print "ECU1 Module Loader."
 print
 
-DBusGMainLoop(set_as_default=True)
-module_loader_ecu1 = ECU1ModuleLoaderService()
+#DBusGMainLoop(set_as_default=True)
+#module_loader_ecu1 = ECU1ModuleLoaderService()
 
 while True:
+    #FIXME: gtk.main_interaction()
     gtk.main_iteration()
