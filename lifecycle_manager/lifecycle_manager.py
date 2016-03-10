e13f7654c08b79492c4dc9aa115b1251227aa051
Ben Nagy <bnagy4@jaguarlandrover.com>
2016-03-09 17:02:47 -0800

    Added more robust logging and changed all print statements to logger equivalents

--- lifecycle_manager/lifecycle_manager.py
+++ lifecycle_manager/lifecycle_manager.py
@@ -11,6 +11,19 @@ import subprocess
 
 import rpyc
 
+import logging
+
+# configure logging
+logFormatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger = logging.getLogger()
+
+fileHandler = logging.FileHandler("logs/{}.log".format(__name__))
+fileHandler.setFormatter(logFormatter)
+logger.addHandler(fileHandler)
+
+consoleHandler = logging.StreamHandler()
+logger.addHandler(consoleHandler)
+
 #
 # Lifecycle manager service
 #
@@ -20,19 +33,19 @@ class LifecycleManager(object):
                          transaction_id,
                          components):
 
-        print "Lifecycle Manager: Got start_components()"
-        print "  Operation Transaction ID: {}".format(transaction_id)
-        print "  Components:               {}".format(", ".join(components))
-        print "---"
+        logger.info("Lifecycle Manager: Got start_components()")
+        logger.info("  Operation Transaction ID: {}".format(transaction_id))
+        logger.info("  Components:               {}".format(", ".join(components)))
+        logger.info("---")
 
         # Simulate starting components
         # TODO: find out how to start a component on the IMC.
-        print "Starting :"
+        logger.info("Starting :")
         for i in components:
-            print "    Starting: {} (3 sec)".format(i)
+            logger.info("    Starting: {} (3 sec)".format(i))
             time.sleep(3.0)
         print
-        print "Done"
+        logger.info("Done")
         swm.send_operation_result(transaction_id,
                                   swm.SWM_RES_OK,
                                   "Started components {}".format(", ".join(components)))
@@ -42,35 +55,35 @@ class LifecycleManager(object):
                         transaction_id,
                         components):
 
-        print "Lifecycle Manager: Got stop_components()"
-        print "  Operation Transaction ID: {}".format(transaction_id)
-        print "  Components:               {}".format(", ".join(components))
-        print "---"
+        logger.info("Lifecycle Manager: Got stop_components()")
+        logger.info("  Operation Transaction ID: {}".format(transaction_id))
+        logger.info("  Components:               {}".format(", ".join(components)))
+        logger.info("---")
 
         # Simulate stopping components
         try:
-            print "Stopping :"
+            logger.info("Stopping :")
             for i in components:
                 #TODO: test whether this actually works on the IMC
-                print "    Stopping: {} (3 sec)".format(i)
+                logger.info("    Stopping: {} (3 sec)".format(i))
                 subprocess.check_call("pkill -f {}".format(i), shell=True)
                 time.sleep(3.0)
             print
-            print "Done"
+            logger.info("Done")
             swm.send_operation_result(transaction_id,
                                       swm.SWM_RES_OK,
                                       "Stopped components {}".format(", ".join(components)))
 
         except subprocess.CalledProcessError as e:
-            print "stop_components() CalledProcessError returncode: {}".format(e.returncode)
-            print str(e)
+            logger.exception("stop_components() CalledProcessError returncode: {}".format(e.returncode))
+            logger.exception(str(e))
             swm.send_operation_result(transaction_id,
                                       swm.SWM_RES_INTERNAL_ERROR,
                                       "Internal_error: {}".format(e))
 
         except Exception as e:
-            print "stop_components() Exception: {}".format(e)
-            print str(e)
+            logger.exception("stop_components() Exception: {}".format(e))
+            logger.exception(str(e))
             swm.send_operation_result(transaction_id,
                                       swm.SWM_RES_INTERNAL_ERROR,
                                       "Internal_error: {}".format(e))
@@ -79,10 +92,10 @@ class LifecycleManager(object):
 
 class LCMgrService(rpyc.Service):
     def on_connect(self):
-        print "A client connected"
+        logger.info("A client connected")
 
     def on_disconnect(self):
-        print "A client disconnected"
+        logger.info("A client disconnected")
 
     def exposed_start_components(self, transaction_id, components):
         """ function to expose start_components over RPyC
@@ -96,13 +109,13 @@ class LCMgrService(rpyc.Service):
 
 
 print
-print "Lifecycle Manager."
+logger.info("Lifecycle Manager.")
 print
 
-print "Initializing LifecycleManager..."
+logger.info("Initializing LifecycleManager...")
 LCMgr = LifecycleManager()
 
 from rpyc.utils.server import ThreadedServer
 t = ThreadedServer(LCMgrService, port = swm.PORT_LCMGR)
-print "Starting LCMgrService ThreadServer on port " +  str(swm.PORT_LCMGR)
+logger.info("Starting LCMgrService ThreadServer on port " +  str(swm.PORT_LCMGR))
 t.start()
